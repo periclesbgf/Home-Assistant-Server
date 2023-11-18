4d52141ebb769b645ebf2c3cf4b9352d5fe80c26
Periclesbgf <pbgf@cesar.school>
2023-11-18 15:01:01 -0300

    data augmentation

---
+++ data_augmentation/data_augmentation_noise.py
@@ -0,0 +1,35 @@
+import os
+import librosa
+import numpy as np
+import soundfile as sf
+
+def add_noise(audio, noise_level=0.0001):
+    noise = noise_level * np.random.randn(len(audio))
+    return audio + (noise/2)
+
+def augment_data_with_noise(input_folder, noise_level=0.005):
+    # Percorra todos os arquivos na pasta de entrada
+    for filename in os.listdir(input_folder):
+        if filename.endswith(".wav"):
+            input_path = os.path.join(input_folder, filename)
+
+            # Carregue o arquivo de áudio usando librosa
+            audio, sr = librosa.load(input_path, sr=None)
+
+            # Aplique a adição de ruído
+            augmented_audio = add_noise(audio, noise_level)
+
+            # Construa o caminho de saída com um sufixo para o nome do arquivo aumentado
+            output_filename = f"augmented_{filename}"
+            output_path = os.path.join(input_folder, output_filename)
+
+            # Salve o áudio aumentado no mesmo diretório que o original
+            sf.write(output_path, augmented_audio, sr)
+
+if __name__ == "__main__":
+    input_folder = "G:/server/Home-Assistant-Server/data_augmentation"
+
+    # Ajuste o nível de ruído conforme necessário
+    noise_level = 0.005
+
+    augment_data_with_noise(input_folder, noise_level)
--- server.py
+++ server.py
@@ -9,6 +9,7 @@ import random
 import string
 import os
 from threading import Thread
+from concurrent.futures import ThreadPoolExecutor
 from pydub import AudioSegment
 
 commands = ['_background_noise_', 'background', 'eden', 'marvin', 'off', 'on']
@@ -17,49 +18,39 @@ loaded_model = models.load_model("./saved")
 
 SERVER_IP = "192.168.1.3"
 SERVER_PORT = 12445
+ESP32_TCP_SERVER_IP = "192.168.1.6"  # Substitua pelo IP do servidor TCP da ESP32
+ESP32_TCP_SERVER_PORT = 12345  # Substitua pela porta do servidor TCP da ESP32
 RECEIVE_DURATION_SECONDS = 1
+RECEIVE_BUFFER_SIZE = 1024
 
 def receive_audio_data():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((SERVER_IP, SERVER_PORT))
-    server_socket.listen(1)
 
-    print(f"Aguardando conexão em {SERVER_IP}:{SERVER_PORT}...")
-    client_socket, client_address = server_socket.accept()
-    print(f"Conexão estabelecida com {client_address}")
+    print(f"Aguardando conexões em {SERVER_IP}:{SERVER_PORT}...")
 
-    while True:
-        string_aleatoria = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
-        wav_file = string_aleatoria + ".wav"
-        received_data = bytearray()
-        start_time = time.time()
+    with ThreadPoolExecutor(max_workers=5) as executor:  # Ajuste o número de workers conforme necessário
+        while True:
+            string_aleatoria = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+            wav_file = string_aleatoria + ".wav"
+            received_data = bytearray()
+            start_time = time.time()
 
-        try:
-            while time.time() - start_time < RECEIVE_DURATION_SECONDS:
-                data = client_socket.recv(1024)
-                if not data:
-                    break
-                received_data.extend(data)
-        except Exception as e:
-            print(f"Erro durante a recepção dos dados: {e}")
+            try:
+                while time.time() - start_time < RECEIVE_DURATION_SECONDS:
+                    data, _ = server_socket.recvfrom(RECEIVE_BUFFER_SIZE)
+                    received_data.extend(data)
+            except Exception as e:
+                print(f"Erro durante a recepção dos dados: {e}")
 
-        if received_data:
-            # Utilizando threads para realizar o salvamento, predição e exclusão
-            thread_save_predict_delete = Thread(
-                target=save_predict_delete,
-                args=(received_data, wav_file)
-            )
-            thread_save_predict_delete.start()
+            if received_data:
+                # Utilizando ThreadPoolExecutor para realizar o salvamento, predição e exclusão
+                executor.submit(save_predict_delete, received_data, wav_file)
 
-    client_socket.close()
     server_socket.close()
-
-    return received_data
 
 def save_predict_delete(data, filename):
     save_audio_data_to_wav(data, filename)
-    #aumento_em_db = 10  # Ajuste o valor conforme necessário
-    #aumentar_volume(filename, filename, aumento_em_db)
     predict_mic(filename)
     delete_file(filename)
 
@@ -73,21 +64,14 @@ def save_audio_data_to_wav(data, filename):
     except Exception as e:
         print(f"Erro ao salvar os dados de áudio como WAV: {e}")
 
-def print_received_data(data):
-    # Aqui, você pode processar ou imprimir os dados recebidos conforme necessário
-    print("Dados recebidos:")
-    print(data)
-
-def main():
-    receive_audio_data()
-    #print_received_data(received_data)
-
 def predict_mic(file):
     audio = record_audio_from_file(file)
     spec = preprocess_audiobuffer(audio)
     prediction = loaded_model(spec)
     label_pred = np.argmax(prediction, axis=1)
     command = commands[label_pred[0]]
+    if command == 'eden':  # Adicione esta função para verificar se o comando predito é "eden"
+        connect_to_esp32_tcp_server()
     print("Predicted:", command)
 
 def delete_file(file):
@@ -96,16 +80,14 @@ def delete_file(file):
     except OSError as e:
         print(f"Erro ao excluir o arquivo {file}: {e}")
 
-def aumentar_volume(input_file, output_file, aumento_em_db):
-    # Carregar o arquivo de áudio
-    audio = AudioSegment.from_wav(input_file)
-
-    # Aumentar o volume em dB
-    audio = audio + aumento_em_db
-
-    # Salvar o novo arquivo de áudio
-    audio.export(output_file, format="wav")
-
+def connect_to_esp32_tcp_server():
+    esp32_tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        esp32_tcp_socket.connect((ESP32_TCP_SERVER_IP, ESP32_TCP_SERVER_PORT))
+        esp32_tcp_socket.send(b'eden')  # Enviar um byte representando "eden"
+        esp32_tcp_socket.close()
+    except Exception as e:
+        print(f"Erro ao conectar e enviar dados para o servidor TCP da ESP32: {e}")
 
 if __name__ == "__main__":
-    main()
+    receive_audio_data()
